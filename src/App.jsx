60798c6c2d39c64e6c941bbda1eee79c7205e556
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2026-06-11 16:24:55 +0600

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import navImg from "./assets/logo.png";
+import coinImg from "./assets/dollar 1.png";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="">
+            <img className=" w-[60px] h-[60px] " src={navImg} alt="" />
+          </a>
+        </div>
+        <div className="flex items-center">
+          <span className=" mr-1">60000000</span>
+          <span>Coin</span>
+          <img src={coinImg} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
